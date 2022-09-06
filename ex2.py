9d03bd3fa88cfca83856a0d79dbbcf90422cf40c
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-06 11:22:11 -0300

    docs: exercicio 2 POO

---
+++ ex2.py
@@ -0,0 +1,33 @@
+# Escreva uma classe “PessoaFisica” e herde Pessoa, crie um método exclusivo para a classe e acesse-o
+
+class Pessoa():
+    def __init__(self, cpf, name, age, rg):
+        #Inicializa os atributos
+        self.cpf = cpf
+        self.name = name
+        self.age = age
+        self.rg = rg
+       
+    def document(self):
+        documento = (f'Nome = {self.name} \nIdade = {self.age} \nCPF = {self.cpf} \nRG = {self.rg}' ) 
+        return documento
+
+
+class PessoaFisica(Pessoa):
+    def __init__(self, cpf, name, age, rg, type):
+        #Não precisa inicializar as que vieram da outra classe, o super inicializa elas
+        self.type = type
+
+        super().__init__(cpf, name, age, rg)
+        
+    def get_type_person(self):
+        if self.type == 'Fisica':
+            return (f'Pessoa Física')
+        else:
+            return (f'Não é Pessoa Física')
+
+person = PessoaFisica('036.837.134-21', 'Nicolle', '23', '9034353684', 'Fisica')
+documento_pessoal = person.document()
+print(documento_pessoal)
+tipo_pessoa = person.get_type_person()
+print(tipo_pessoa)
